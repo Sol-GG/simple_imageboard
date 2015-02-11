cf138f0b0bef15a2c955944bad33df5eb9491537
Ilia Gavrilov <sandik@mail.ru>
2015-02-11 18:00:47 +0400

    Post model added

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,7 @@
 class Post < ActiveRecord::Base
+	belongs_to :topic
+	validates :topic, presence: true 
+	validates :topic_id, presence: true 
+	default_scope -> { order('updated_at ASC') }
+	validates :content, presence: true, length: { maximum: 1600 }
 end
--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,4 +1,5 @@
 class Topic < ActiveRecord::Base
+	has_many :posts
 	belongs_to :board
 	validates :board, presence: true 
 	default_scope -> { order('updated_at DESC') }
